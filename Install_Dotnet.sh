024532edfdf6e6f13768398319db7eee669107d4
Andrew Cavallo <65136929+Awire9966@users.noreply.github.com>
2023-03-01 19:58:08 -0500

    Official Dotnet Install script

---
+++ Install_Dotnet.sh
@@ -0,0 +1,5 @@
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel STS
+echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
+echo 'export PATH=$PATH:$HOME/.dotnet' >> ~/.bashrc
+source ~/.bashrc
+dotnet --version
